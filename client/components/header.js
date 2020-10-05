60bccd85b351ea93aae4787e90b5f180ea6a5298
Yury <y.rudko@gmail.com>
2020-10-05 12:13:27 +0300

    corrected3: #menu

--- client/components/header.js
+++ client/components/header.js
@@ -15,18 +15,19 @@ const Header = () => {
         >
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg>
-        <button
-          type="button"
-          id="toggle-button"
-          className="font-semibold text-xl tracking-tight"
-          onClick={() => toggle(!toggled)}
-        >
-          Tailwind CSS
-        </button>
+        <a href="https://www.google.com/">
+          <button
+            type="button"
+            id="toggle-button"
+            className="font-semibold text-xl tracking-tight"
+            onClick={() => toggle(!toggled)}
+          >
+            Tailwind CSS
+          </button>
+        </a>
       </div>
       {toggled && (
-      <div id="menu" className="block">
-        <a href="https://www.google.com/">
+        <div id="menu" className="block">
           <button
             type="button"
             className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
@@ -40,8 +41,7 @@ const Header = () => {
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
-        </a>
-      </div>
+        </div>
       )}
     </nav>
   )
